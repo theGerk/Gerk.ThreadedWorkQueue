42131d1148c7ff92c525f374dbb61c4b06176cf6
Benji Altman <benjidaltman@gmail.com>
2024-08-20 12:28:36 -0500

    Big refactor

--- Gerk.ThreadedWorkQueue/ThreadedWorkQueue.cs
+++ Gerk.ThreadedWorkQueue/ThreadedWorkQueue.cs
@@ -4,25 +4,11 @@ using System.Threading.Tasks;
 
 namespace Gerk.ThreadedWorkQueue
 {
-	/// <summary>
-	/// Can be passed elements of <typeparamref name="T"/> and will use a single thread to execute <see cref="DigestItem(T)"/> function on each item. The thread will only be spun up when work is enqueued and will exist until all work is complete at which point it will exit. Sometimes like in the case of writing to a file a "flush" might be useful so the <see cref="FlushBuffer"/> can be overriden to have any logic that should happen infrequently when a batch of items are done being procesesed.
-	/// </summary>
-	/// <typeparam name="T">A type to be put into the queue and consumed.</typeparam>
-	public abstract class ThreadedWorkQueue<T>
+	public abstract class ThreadedWorkQueueBase<T>
 	{
-		/// <summary>
-		/// <see langword="override"/> to define how each item should be handled.
-		/// </summary>
-		/// <param name="item">The <typeparamref name="T"/> to be worked on.</param>
-		abstract protected void DigestItem(T item);
-		/// <summary>
-		/// Defaults to nothing. <see langword="override"/> to define logic that is run infrequently when the queue empties.
-		/// </summary>
-		virtual protected void FlushBuffer() { }
-
 
 		// Used by EmptyQueue
-		private TaskCompletionSource<bool> completionEvent;
+		internal TaskCompletionSource<bool> completionEvent;
 
 		/// <summary>
 		/// Used to wait for the queue to complete its work.
@@ -40,40 +26,29 @@ namespace Gerk.ThreadedWorkQueue
 			return completionEvent.Task;
 		}
 
-		private readonly ConcurrentQueue<T> internalQueue = new ConcurrentQueue<T>();
+		internal readonly ConcurrentQueue<T> internalQueue = new ConcurrentQueue<T>();
 
-		private bool threadExpired = true;
-		private readonly object threadExpiredLock = new object();
+		internal bool threadExpired = true;
+		internal readonly object threadExpiredLock = new object();
 
-		private void ThreadFunc()
+		internal bool EndThreadIfNeeded()
 		{
-			for (; ; )
+			lock (threadExpiredLock)
 			{
-				do
+				threadExpired = internalQueue.IsEmpty;
+				if (threadExpired)
 				{
-					if (internalQueue.TryDequeue(out T item))
+					if (completionEvent != null)
 					{
-						DigestItem(item);
-					}
-				} while (!internalQueue.IsEmpty);
-
-				FlushBuffer();
-
-				lock (threadExpiredLock)
-				{
-					threadExpired = internalQueue.IsEmpty;
-					if (threadExpired)
-					{
-						if (completionEvent != null)
-						{
-							completionEvent.TrySetResult(true);
-							completionEvent = null;
-						}
-						break;
+						completionEvent.TrySetResult(true);
+						completionEvent = null;
 					}
 				}
 			}
+			return threadExpired;
 		}
+
+		internal abstract void ThreadFunc();
 
 		/// <summary>
 		/// Add an item to be handled. Will spin up the thread if one isn't currently running.
@@ -96,11 +71,47 @@ namespace Gerk.ThreadedWorkQueue
 	}
 
 	/// <summary>
+	/// Can be passed elements of <typeparamref name="T"/> and will use a single thread to execute <see cref="DigestItem(T)"/> function on each item. The thread will only be spun up when work is enqueued and will exist until all work is complete at which point it will exit. Sometimes like in the case of writing to a file a "flush" might be useful so the <see cref="FlushBuffer"/> can be overriden to have any logic that should happen infrequently when a batch of items are done being procesesed.
+	/// </summary>
+	/// <typeparam name="T">A type to be put into the queue and consumed.</typeparam>
+	public abstract class ThreadedWorkQueue<T> : ThreadedWorkQueueBase<T>
+	{
+		/// <summary>
+		/// <see langword="override"/> to define how each item should be handled.
+		/// </summary>
+		/// <param name="item">The <typeparamref name="T"/> to be worked on.</param>
+		abstract protected void DigestItem(T item);
+		/// <summary>
+		/// Defaults to nothing. <see langword="override"/> to define logic that is run infrequently when the queue empties.
+		/// </summary>
+		virtual protected void FlushBuffer() { }
+
+		internal override void ThreadFunc()
+		{
+			for (; ; )
+			{
+				do
+				{
+					if (internalQueue.TryDequeue(out T item))
+					{
+						DigestItem(item);
+					}
+				} while (!internalQueue.IsEmpty);
+
+				FlushBuffer();
+
+				if (EndThreadIfNeeded())
+					break;
+			}
+		}
+	}
+
+	/// <summary>
 	/// Can be passed elements of <typeparamref name="T"/> and will use a single thread to execute <see cref="DigestItem(T, ref PassAhead)"/> function on each item. The thread will only be spun up when work is enqueued and will exist until all work is complete at which point it will exit. Sometimes like in the case of writing to a file a "flush" might be useful so the <see cref="FlushBuffer"/> can be overriden to have any logic that should happen infrequently when a batch of items are done being procesesed.
 	/// </summary>
 	/// <typeparam name="T">A type to be put into the queue and consumed.</typeparam>
 	/// <typeparam name="PassAhead">A type to hold information that is passed between calls of <see cref="DigestItem(T, ref PassAhead)"/> and <see cref="FlushBuffer(PassAhead)"/>. The first call of <see cref="DigestItem(T, ref PassAhead)"/> after a <see cref="FlushBuffer(PassAhead)"/> will always be <see langword="default"/>.</typeparam>
-	public abstract class ThreadedWorkQueue<T, PassAhead>
+	public abstract class ThreadedWorkQueue<T, PassAhead> : ThreadedWorkQueueBase<T>
 	{
 		/// <summary>
 		/// <see langword="override"/> to define how each item should be handled.
@@ -117,32 +128,7 @@ namespace Gerk.ThreadedWorkQueue
 			return Task.FromResult(true);
 		}
 
-
-		// Used by EmptyQueue
-		private TaskCompletionSource<bool> completionEvent;
-
-		/// <summary>
-		/// Used to wait for the queue to complete its work.
-		/// </summary>
-		/// <remarks>May be useful for debugging.</remarks>
-		/// <returns>A task that yields when the queue is empty.</returns>
-		public Task EmptyQueue()
-		{
-			lock (threadExpiredLock)
-				if (threadExpired)
-					return Task.FromResult(true);
-				else if (completionEvent == null)
-					completionEvent = new TaskCompletionSource<bool>();
-
-			return completionEvent.Task;
-		}
-
-		private readonly ConcurrentQueue<T> internalQueue = new ConcurrentQueue<T>();
-
-		private bool threadExpired = true;
-		private readonly object threadExpiredLock = new object();
-
-		private void ThreadFunc()
+		internal override void ThreadFunc()
 		{
 			for (; ; )
 			{
@@ -157,38 +143,8 @@ namespace Gerk.ThreadedWorkQueue
 
 				FlushBuffer(passAlong);
 
-				lock (threadExpiredLock)
-				{
-					threadExpired = internalQueue.IsEmpty;
-					if (threadExpired)
-					{
-						if (completionEvent != null)
-						{
-							completionEvent.TrySetResult(true);
-							completionEvent = null;
-						}
-						break;
-					}
-				}
-			}
-		}
-
-		/// <summary>
-		/// Add an item to be handled. Will spin up the thread if one isn't currently running.
-		/// </summary>
-		/// <param name="item">The data for the item.</param>
-		public void EnqueueWork(T item)
-		{
-			bool createNewThread;
-			internalQueue.Enqueue(item);
-			lock (threadExpiredLock)
-			{
-				createNewThread = threadExpired;
-				threadExpired = false;
-			}
-			if (createNewThread)
-			{
-				new Thread(ThreadFunc).Start();
+				if (EndThreadIfNeeded())
+					break;
 			}
 		}
 	}
